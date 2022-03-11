0f9af6c0ce9e55f77f849e6103941576d51594e4
philipobiorah <philip.obiorah@outlook.com>
2022-03-12 00:58:25 +0100

    Add 6-print nmber

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+printf("\n");
+return (0);
+}
