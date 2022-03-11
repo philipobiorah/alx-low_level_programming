502066a8354ae181a29a8e6dfbf089b788035252
philipobiorah <philip.obiorah@outlook.com>
2022-03-11 15:47:35 +0100

    Update 0-positive..

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,12 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is %s\n",n, "positive");
+printf("%d is %s\n", n, "positive");
 }
 else if (n == 0)
 {
-printf("%d is %s\n",n, "zero");
+printf("%d is %s\n", n, "zero");
 }
 else
-printf("%d is %s\n",n, "negative");
+printf("%d is %s\n", n, "negative");
 }
