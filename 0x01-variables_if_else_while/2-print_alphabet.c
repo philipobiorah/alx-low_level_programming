7d0bceee4fa8da3ba0a87d55c6924e0a116543d0
philipobiorah <philip.obiorah@outlook.com>
2022-03-11 22:42:07 +0100

    Add 2-print_alpahbet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+printf("\n");
+}
+}
