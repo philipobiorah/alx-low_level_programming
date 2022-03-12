ea35031966d830638abb25caefc9b19d0be49343
philipobiorah <philip.obiorah@outlook.com>
2022-03-12 01:07:46 +0100

    Update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,16 +3,21 @@
 #include <time.h>
 /**
  * main - Entry point
+ *
+ * Description: prints all single digit number of base 10
+ * starting from 0
+ *
  * Return: 0
  */
-
 int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-putchar(i + '0');
-}
-printf("\n");
-return (0);
+	int b;
+
+	for (b = 0; b < 10;  b++)
+	{
+		putchar(b + '0');
+	}
+	putchar('\n');
+
+	return (0);
 }
