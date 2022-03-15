34ff339a27cc8fbe7e7936f71ec507ba055d0419
philipobiorah <philip.obiorah@outlook.com>
2022-03-15 17:32:58 +0100

    Add void print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet  - This prints the alphabet a - z
+ */
+void print_alphabet_x10(void)
+{
+int count ;
+for (count = 0; count < 10; count++)
+{
+char i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}    
+
+}
