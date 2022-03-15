51b31dd81c4d5dc5496a4f1bb930887efdcf5fd6
philipobiorah <philip.obiorah@outlook.com>
2022-03-15 14:00:36 +0100

    Modify 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * Main method : entry point
- * Returns 0 (success)
+ * main - This Function prints _putchar using the local header file
+ * Return : 0 (Success)
  */
 
-int main()
+int main(void)
 {
-//output a line
-printf("_putchar\n");    
-}
+    char c[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+    unsigned int i;
 
+    for(i = 0; i < sizeof(c); i++)
+    {
+        _putchar(c[i]);
+    }
+    _putchar('\n');
+    
+    return (0);
+}
