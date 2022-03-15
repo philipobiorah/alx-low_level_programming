0f4dce9c613b4db55e32dc35955df38c5f5eb8c7
philipobiorah <philip.obiorah@outlook.com>
2022-03-15 23:30:40 +0100

    Add 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: computes the absolute value of an integer..
+ * @n: the integer value it receives
+ * Return: the absolute value of the  integer  argument,  of  the
+ * appropriate integer type for the function.
+ */
+
+int _abs(int n)
+{
+return (abs(n));
+}
+
+
+
+
