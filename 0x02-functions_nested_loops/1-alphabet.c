e7e001f926f34020b2a77765ac6fe8eb0ad3ceec
philipobiorah <philip.obiorah@outlook.com>
2022-03-15 15:37:50 +0100

    I sometimes suffer form insomnaia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+// char c[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+// unsigned int i;
+for (char i = 'a'; i <  'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+
+return (0);
+}
